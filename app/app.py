9527806fc49811e8af2b82f5eeef6a746c7c355c
prajwal3014 <prajwalsharma409@gmail.com>
2021-08-25 01:41:56 +0530

    new

--- app/app.py
+++ app/app.py
@@ -1,9 +1,62 @@
 from flask import Flask, render_template, request
+import psycopg2
+import json
 
 app = Flask(__name__)
 
+connection = psycopg2.connect(database='postgres', user='postgres', password='admin', host='localhost')
+obj = connection.cursor()
+
 @app.route("/")
 def home() :
-    return render_template("home.html")
+    return render_template("register.html")
 
-app.run(debug=True)
+@app.route("/tologin")
+def to_login() :
+    return render_template("login.html")
+
+@app.route("/register", methods = ['POST', 'GET'])
+def register() :
+    u_name = request.form['uname']
+    user_name = u_name.lower()
+    user_pass = request.form['upass']
+    u_file = 'uname.json'
+    d_file = 'details.json'
+    with open(u_file, 'r') as user_file :
+        user_list = json.load(user_file)
+    with open(d_file, 'r') as n :
+        user_dict = json.load(n)
+    
+    if user_name not in user_list :
+        user_list.append(user_name)
+        user_dict[user_name] = user_pass
+        with open(u_file, 'w') as u :
+            json.dump(user_list, u, indent=4)
+        with open(d_file, 'w') as n1 :
+            json.dump(user_dict, n1, indent=4)
+        obj.execute(""" insert into app values ('{0}', '{1}') """.format(user_name, user_pass))
+        connection.commit()
+        return render_template("login.html")
+    elif user_name in user_list :
+        return render_template("register.html", msg = "Username Already taken...!")
+
+@app.route("/login", methods = ['POST', 'GET'])
+def login() :
+    u_name = request.form['uname']
+    user_name = u_name.lower()
+    user_pass = request.form['upass']
+    d_file = 'details.json'
+    with open(d_file, 'r') as n :
+        user_dict = json.load(n)
+    user_list = list(user_dict.keys())
+    pass_list = list(user_dict.values())
+    count_name = -1
+    count_pass = -2
+    if user_pass in pass_list :
+        count_pass = pass_list.index(user_pass)
+    if user_name in user_list :
+        count_name = user_list.index(user_name)
+    if count_name != count_pass :
+        return render_template("login.html", msg = "Incorrect username or password, please check again...!")
+    elif count_name == count_pass :
+        return render_template("user.html", msg = user_name, list = user_list)
--- app/test.py
+++ app/test.py
@@ -1,23 +1,58 @@
-import psycopg2
+# import psycopg2
 
-connection = psycopg2.connect(database='postgres', user='postgres', password='admin', host='localhost')
-print("Connection is opened...!")
-obj = connection.cursor()
+# connection = psycopg2.connect(database='postgres', user='postgres', password='admin', host='localhost')
+# print("Connection is opened...!")
+# obj = connection.cursor()
 
-try :
-    # obj.execute(""" create table app2 (
-    #                 uname varchar(255),
-    #                 upass varchar(255)
-    #             ); """)
-    # obj.execute(""" insert into app2 values ('prajwal', '12prajwal'); """)
-    # obj.execute(""" insert into app2 values ('ayush', '12ayush'); """)
-    obj.execute(""" select * from app """)
-    results = obj.fetchall()
-    connection.commit()
-    print(results)
-    print("Query successfully executed...!")
-except :
-    print("There some problem in executing query...!")
-finally :
-    connection.close()
-    print("Connection is closed...!")
+# try :
+#     # obj.execute(""" create table app2 (
+#     #                 uname varchar(255),
+#     #                 upass varchar(255)
+#     #             ); """)
+#     # obj.execute(""" insert into app2 values ('prajwal', '12prajwal'); """)
+#     # obj.execute(""" insert into app2 values ('ayush', '12ayush'); """)
+#     obj.execute(""" select * from app """)
+#     results = obj.fetchall()
+#     connection.commit()
+#     print(results)
+#     print("Query successfully executed...!")
+# except :
+#     print("There some problem in executing query...!")
+# finally :
+#     connection.close()
+#     print("Connection is closed...!")
+import json
+
+# with open('uname.json', 'r') as userfile :
+#     uname = json.load(userfile)
+# with open('details.json', 'r') as userfile :
+#     user_dict = json.load(userfile)
+# x="prajju"
+# y="prajju"
+# count_name = -1
+# count_pass = -2
+# upass = list(user_dict.values())
+# if x in uname :
+#     count_name = uname.index(x)
+# if y in upass :
+#     count_pass = upass.index(y)
+# if count_name == count_pass :
+#     print("Yes")
+# else :
+#     print("No")
+u_name = input()
+user_name = u_name.lower()
+user_pass = input()
+d_file = 'details.json'
+with open(d_file, 'r') as n :
+    user_dict = json.load(n)
+user_list = list(user_dict.keys())
+pass_list = list(user_dict.values())
+if user_pass in pass_list :
+    count_pass = pass_list.index(user_pass)
+if user_name in user_list :
+    count_name = user_list.index(user_name)
+if count_name != count_pass :
+    print("invalid username or password")
+elif count_name == count_pass :
+    print("welcome {0}".format(user_name))
