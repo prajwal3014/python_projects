5cd8e5480f5d6630f415da827b2e3eedf5e68172
prajwal3014 <prajwalsharma409@gmail.com>
2021-08-25 17:58:38 +0530

    new

--- app/app.py
+++ app/app.py
@@ -7,6 +7,8 @@ app = Flask(__name__)
 connection = psycopg2.connect(database='postgres', user='postgres', password='admin', host='localhost')
 obj = connection.cursor()
 
+lst = []
+
 # Home
 @app.route("/")
 def home() :
@@ -18,9 +20,24 @@ def to_login() :
     return render_template("login.html")
 
 #Logout
-@app.route("/logout")
+@app.route("/logout", methods = ['POST', 'GET'])
 def logout() :
-    return render_template("login.html", msg = "Logout Successfully")
+    return render_template("login.html", msg = "Logout Successfully...!")
+
+#Navigate to Create room page
+@app.route("/create", methods = ['POST', 'GET'])
+def create() :
+    return render_template("create_room.html")
+
+#Navigate to Join room page
+@app.route("/join", methods = ['POST', 'GET'])
+def join() :
+    return render_template("join_room.html")
+
+#Leave Chat
+@app.route("/leave", methods = ['POST', 'GET'])
+def leave() :
+    return render_template("user.html", msg = "Exited from the recent chat room...!")
 
 #Register page working
 @app.route("/register", methods = ['POST', 'GET'])
@@ -34,7 +51,6 @@ def register() :
         user_list = json.load(user_file)
     with open(d_file, 'r') as n :
         user_dict = json.load(n)
-    
     if user_name not in user_list :
         user_list.append(user_name)
         user_dict[user_name] = user_pass
@@ -54,6 +70,7 @@ def login() :
     u_name = request.form['uname']
     user_name = u_name.lower()
     user_pass = request.form['upass']
+    lst.append(user_name)
     d_file = 'details.json'
     with open(d_file, 'r') as n :
         user_dict = json.load(n)
@@ -68,18 +85,36 @@ def login() :
     if count_name != count_pass :
         return render_template("login.html", msg = "Incorrect username or password, please check again...!")
     elif count_name == count_pass :
-        return render_template("user.html", msg = user_name, list = user_list)
-
-#Navigate to Create room page
-@app.route("/create")
-def create() :
-    return render_template("create_room.html")
+        return render_template("user.html")
 
 #Create room page working
 @app.route("/create_room", methods = ['POST', 'GET'])
 def create_room() :
     room_name = request.form['create']
     room_file = "room.json"
+    user_name = lst.pop()
+    lst.append(user_name)
     with open(room_file, 'r') as f :
-        room_list = json.load(room_file)
-    
+        room_list = json.load(f)
+    if room_name in room_list :
+        return render_template("create_room.html", msg = "Room already exists...!")
+    elif room_name not in room_list :
+        room_list.append(room_name)
+        with open(room_file, 'w') as c :
+            json.dump(room_list, c, indent=4)
+        return render_template("chat.html", user = user_name, room = room_name, lst = room_list)
+
+#Join room working
+@app.route("/join_room", methods = ['POST', 'GET'])
+def join_room() :
+    room_name = request.form['join']
+    user_name = lst.pop()
+    lst.append(user_name)
+    room_file = "room.json"
+    with open(room_file, 'r') as f :
+        room_list = json.load(f)
+    if room_name in room_list :
+        return render_template("chat.html", user = user_name, room = room_name, lst = room_list)
+
+if __name__ == '__main__' :
+    app.run() 
