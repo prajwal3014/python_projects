9111a2e152fcd8510aa58c7f29807497da44ba42
prajwal3014 <prajwalsharma409@gmail.com>
2021-12-01 11:12:48 +0530

    new

--- chat_app_project/app.py
+++ chat_app_project/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_socketio import SocketIO, join_room, leave_room
 from flask_login import LoginManager, login_user, login_required, logout_user
 import json
