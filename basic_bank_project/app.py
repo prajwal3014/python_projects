07a79164a8149aa4ec392724643a10546b83a4b1
prajwal3014 <prajwalsharma409@gmail.com>
2021-12-02 01:03:19 +0530

    new

--- basic_bank_project/app.py
+++ basic_bank_project/app.py
@@ -1,1 +1,11 @@
-from flask import render_template
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.secret_key = "GRIP BANK PROJECT BY PRAJWAL SHARMA"
+
+@app.route('/')
+def index() :
+    return render_template('index.html')
+
+if __name__ == "__main__" :
+    app.run(debug=True)
