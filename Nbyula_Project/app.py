f99b40beb5e8699708e71f8034464a1ef2d3ba34
prajwal3014 <70304360+prajwal3014@users.noreply.github.com>
2022-09-17 19:41:47 +0530

    new

--- Nbyula_Project/app.py
+++ Nbyula_Project/app.py
@@ -6,12 +6,24 @@ app = Flask(__name__)
 app.secret_key = "THIS IS A NBYULA PROJECT"
 
 @app.route('/')
-def home() :
+def sign_in() :
     return render_template('login.html')
 
 @app.route('/sign-up')
-def sign_in() :
+def sign_up() :
     return render_template('register.html')
+
+@app.route('/home')
+def home() :
+    return render_template('home.html')
+
+@app.route('/book')
+def book() :
+    return render_template('book_appointments.html')
+
+@app.route('/change_password')
+def change_password() :
+    return render_template('change_password.html')
 
 if __name__ == "__main__" :
     app.run(debug=TRUE)
