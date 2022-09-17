cacebd9fa6a2ed3ca88dc4446e3de3bea901f093
prajwal3014 <70304360+prajwal3014@users.noreply.github.com>
2022-09-17 17:24:24 +0530

    new

--- Nbyula_Project/app.py
+++ Nbyula_Project/app.py
@@ -1,1 +1,17 @@
-print("hello world")
+from pickle import TRUE
+from flask import Flask, render_template, request, redirect, url_for
+from flask_login import LoginManager, login_user, login_required, logout_user
+
+app = Flask(__name__)
+app.secret_key = "THIS IS A NBYULA PROJECT"
+
+@app.route('/')
+def home() :
+    return render_template('login.html')
+
+@app.route('/sign-up')
+def sign_in() :
+    return render_template('register.html')
+
+if __name__ == "__main__" :
+    app.run(debug=TRUE)
